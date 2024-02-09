c0b46b17b63b76b7ec60d642f72552f53ee4850b
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-02-09 11:33:36 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodePractice_GoTosAndLabels_AdamIshak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
